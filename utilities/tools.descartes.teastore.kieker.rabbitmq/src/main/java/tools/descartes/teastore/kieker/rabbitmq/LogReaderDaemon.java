d120878b8ee825f213b2ffc640478f54c6dafd45
AlpTorac <a.torac@hotmail.com>
2024-12-23 10:26:00 +0100

    Squashed v1.0.0 - v1.1.0
    Changed indentation in LogReaderDaemon

--- utilities/tools.descartes.teastore.kieker.rabbitmq/src/main/java/tools/descartes/teastore/kieker/rabbitmq/LogReaderDaemon.java
+++ utilities/tools.descartes.teastore.kieker.rabbitmq/src/main/java/tools/descartes/teastore/kieker/rabbitmq/LogReaderDaemon.java
@@ -1,11 +1,7 @@
 package tools.descartes.teastore.kieker.rabbitmq;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
 import kieker.analysis.AnalysisController;
 import kieker.analysis.IAnalysisController;
-import kieker.analysis.exception.AnalysisConfigurationException;
 import kieker.analysis.plugin.reader.amqp.AmqpReader;
 import kieker.analysis.plugin.reader.amqp.ChunkingAmqpReader;
 import kieker.analysis.plugin.reader.newio.RawDataReaderPlugin;
@@ -19,29 +15,26 @@ public class LogReaderDaemon implements Runnable {
 	@Override
 	public void run() {
 		final IAnalysisController analysisInstance = new AnalysisController();
-		Configuration configuration = new Configuration();
+		try {
+			Configuration configuration = new Configuration();
 
-		configuration.setProperty(RawDataReaderPlugin.CONFIG_PROPERTY_READER, "kieker.analysis.plugin.reader.amqp.ChunkingAmqpReader");
-		configuration.setProperty(RawDataReaderPlugin.CONFIG_PROPERTY_DESERIALIZER, "kieker.analysis.plugin.reader.newio.deserializer.BinaryDeserializer");
+			configuration.setProperty(RawDataReaderPlugin.CONFIG_PROPERTY_READER, "kieker.analysis.plugin.reader.amqp.ChunkingAmqpReader");
+			configuration.setProperty(RawDataReaderPlugin.CONFIG_PROPERTY_DESERIALIZER, "kieker.analysis.plugin.reader.newio.deserializer.BinaryDeserializer");
 
-		configuration.setProperty(ChunkingAmqpReader.CONFIG_PROPERTY_URI, URI);
-		configuration.setProperty(ChunkingAmqpReader.CONFIG_PROPERTY_HEARTBEAT, 0);
-		configuration.setProperty(ChunkingAmqpReader.CONFIG_PROPERTY_QUEUENAME, QUEUENAME);
+			configuration.setProperty(ChunkingAmqpReader.CONFIG_PROPERTY_URI, URI);
+			configuration.setProperty(ChunkingAmqpReader.CONFIG_PROPERTY_HEARTBEAT, 0);
+			configuration.setProperty(ChunkingAmqpReader.CONFIG_PROPERTY_QUEUENAME, QUEUENAME);
 
-		RawDataReaderPlugin reader = new RawDataReaderPlugin(configuration, analysisInstance);
-        final LogConsumer consumer = new LogConsumer(new Configuration(), analysisInstance);
+			RawDataReaderPlugin reader = new RawDataReaderPlugin(configuration, analysisInstance);
+        		final LogConsumer consumer = new LogConsumer(new Configuration(), analysisInstance);
  
-        try {
-            analysisInstance.connect(reader, AmqpReader.OUTPUT_PORT_NAME_RECORDS, consumer,
-                    LogConsumer.INPUT_PORT_NAME);
-            analysisInstance.run();
-        } catch (final AnalysisConfigurationException e) {
-            final StringWriter sw = new StringWriter();
-            final PrintWriter pw = new PrintWriter(sw);
-            e.printStackTrace(pw);
-            final String sStackTrace = sw.toString();
-            throw new IllegalStateException(sStackTrace);
-        }
+        		analysisInstance.connect(reader, AmqpReader.OUTPUT_PORT_NAME_RECORDS, consumer, LogConsumer.INPUT_PORT_NAME);
+            		analysisInstance.run();
+		} catch (Exception e) {
+        		System.out.println("AMQP Reader was interupted, probably due to reset");
+		} finally {
+			analysisInstance.terminate();
+		}
 	}
 
 }
